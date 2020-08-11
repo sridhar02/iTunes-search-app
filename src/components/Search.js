4fe60b1fa0819eedc5e1f5053ee9a26638ea1723
sridhar02 <kattasridhar02@gmail.com>
2020-08-12 00:04:12 +0530

    search Component

---
+++ src/components/Search.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+
+import { Input } from "antd";
+const { Search } = Input;
+
+
+
+export default function SearchInput({onItemSearch}) {
+
+
+  return (
+    <>
+      <div style={{ margin: "30px 20px" }}>
+        <Search
+          placeholder="input search text"
+          enterButton="Search"
+          size="large"
+          // onChange={(e) => setSearchTerm(e.target.value)}
+          onSearch={(value) => onItemSearch(value)}
+        />
+      </div>
+    </>
+  );
+}
