1f92ab203137efe7aef4000a4cf1abbe733bcaea
sridhar02 <kattasridhar02@gmail.com>
2020-08-12 00:03:06 +0530

    main app components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { useState } from "react";
+import { create } from "apisauce";
+
+import "./app.css";
+import ItemsList from "./components/ItemsList";
+import SearchInput from "./components/Search.js";
+
+const api = create({
+  baseUrl: "https://itunes.apple.com",
+});
 
 function App() {
+  const [searchResults, setSearchResults] = useState([]);
+
+  function onItemSearch(value) {
+    api
+      .get(
+        `https://cors-anywhere.herokuapp.com/https://itunes.apple.com/search?term=${value}`
+      )
+      .then((response) => setSearchResults(response.data.results))
+      .catch((error) => alert(error));
+  }
   return (
-    <div className="App">
-      <h1>Hello World!</h1>
-    </div>
+    <>
+          <SearchInput onItemSearch={onItemSearch} />
+          <ItemsList searchResults={searchResults} />
+    </>
   );
 }
 
